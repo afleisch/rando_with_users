0cba2ceb3a679c8e099d9faa2f7243822718a2e6
afleisch <afleisch@gmail.com>
2014-01-27 20:50:15 -0800

    added #show and #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
   end
 
   def show
+  	id = params[:id]
+  	@user=User.find(id)
   end
 
   def create
+  	@user=User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Welcome to Rando!"
+      #sign_in @user
+      redirect_to @user
+    else
+      render'new'
+    end
   end
 end
