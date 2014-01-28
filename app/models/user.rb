f912d187ae9837eafd8b749497849b6ce1b92f61
afleisch <afleisch@gmail.com>
2014-01-28 09:47:36 -0800

    added remember token attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_token
   has_secure_password
+
+before_save { |user| user.email = email.downcase }
+before_save :create_remember_token
+
+private
+def create_remember_token
+  self.remember_token = SecureRandom.urlsafe_base64
+end
+
 end
