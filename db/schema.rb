3ea0fdc1458ee185c94fcc9b1271f87141345ce4
afleisch <afleisch@gmail.com>
2014-01-28 09:48:27 -0800

    added remember_token column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128034758) do
+ActiveRecord::Schema.define(version: 20140128165505) do
 
   create_table "urls", force: true do |t|
     t.string   "link"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20140128034758) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
